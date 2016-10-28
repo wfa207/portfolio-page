84533a4fa96b4eb59ee51225d15ad886292e5afa
Wes <wfa207@gmail.com>
2016-10-28 18:19:14 -0400

    fixed up deploy script

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@
 
 if (process.env.NODE_ENV === 'production') {
   let child_process = require('child_process');
-  child_process.exec('webpack -p --config webpack.production.config.js', (err, stdout, stderr) {
+  child_process.exec('webpack -p --config webpack.production.config.js', (err, stdout, stderr) => {
     console.log('stdout: ', stdout);
     console.log('stderr: ', stderr);
     if (err !== null) { console.log('exec error:', err) }
