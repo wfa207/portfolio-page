3e2530c97059e50e5b7993ea4e1f0d3586e4978f
Wes <wfa207@gmail.com>
2016-10-28 18:13:50 -0400

    added in continuous integration features

--- server/app.js
+++ server/app.js
@@ -3,12 +3,32 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 7000;
+const httpProxy = require('http-proxy');
+const webpackConfig = require('../webpack.config.js');
+
+let proxy = httpProxy.createProxyServer();
 let app = express();
 
-app.use('/static', express.static(path.join(__dirname, '../static')));
+const isProduction = process.env.NODE_ENV === 'production';
+const port = isProduction ? process.env.PORT : 7000;
+const publicBuildPath = webpackConfig.output.publicPath + '*';
+
+if (isProduction) {
+  app.use('/static', express.static(path.join(__dirname, '../static')));
+}
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+if (!isProduction) {
+  const devServer = require('./webpackDevServer');
+  devServer();
+
+  app.use('/', (req, res) => {
+    proxy.web(req, res, {
+      target: 'http://localhost:8080'
+    });
+  });
+}
 
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '../index.html')));
 
@@ -17,6 +37,11 @@ app.use((err, req, res, next) => {
   res.status(500).send(err);
 });
 
+proxy.on('error', (err) => {
+  console.log('Could not connect to proxy; ERROR:');
+  console.error(err);
+});
+
 app.listen(port, function() {
-  console.log('Server is listening on port ' + port + '!');
+  console.log('Server is listening on port ' + port);
 });
