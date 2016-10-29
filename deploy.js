58cd2714d2688e3e751a9c415e4cadcc4551be21
Wes <wfa207@gmail.com>
2016-10-29 12:00:12 -0400

    tidied up deploy script

--- deploy.js
+++ deploy.js
@@ -3,8 +3,8 @@
 if (process.env.NODE_ENV === 'production') {
   let child_process = require('child_process');
   child_process.exec('webpack -p --config webpack.production.config.js', (err, stdout, stderr) => {
-    console.log('stdout: ', stdout);
-    console.log('stderr: ', stderr);
+    console.log('stdout: ', stdout || 'null');
+    console.log('stderr: ', stderr || 'null');
     if (err !== null) { console.log('exec error:', err) }
   });
 };
